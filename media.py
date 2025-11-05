86db7f6175a794eb0556638a46d9b3db68e129a6
LeonardoDaV1nc1 <leo.dvx@gmail.com>
2025-11-05 11:05:14 -0300

    Adicionamos verificacao de aprovacao

--- media.py
+++ media.py
@@ -4,8 +4,11 @@ nota2 = float(input("Digite a segunda nota: "))
 
 media = (nota1 + nota2)/2
 
-if media > 5:
+if media > 7:
     print(f"{aluno} passou sua nota {media}")
 
+elif media == 7:
+    print(f"{aluno} ficou na recuperacao {media}")
+
 else:
-    print(f"{aluno} nao atingiu a media {media}")
+    print(f"{aluno} reprovou {media}")
